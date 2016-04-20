81868b17ca3d1ef88a3de82618cffc3b945a355b
Ondrej Odchazel <hypertornado@gmail.com>
2016-04-20 18:59:43 +0200

    changed texts

--- public/script.js
+++ public/script.js
@@ -2,28 +2,38 @@ window.onload = function() {
 
 }
 
+var map;
+
 function initMap() {
   var mapDiv = document.getElementById('map');
-
-  //AIzaSyDgsEoKEfZJZJtwZe8_YtQ3CQFnOZ1xVuI
 
 
   var patroPos = {lat: 50.2015177, lng: 14.8392227};
 
-  var map = new google.maps.Map(mapDiv, {
+  map = new google.maps.Map(mapDiv, {
     center: patroPos,
     zoom: 11
   });
 
-  var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7C004375");
 
-  var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-  iconBase = "/logo/logo.png";
+  addPinToMap(1,50.1646768,14.8865066, "Restaurace Na Jitrách");
+  addPinToMap(2,50.2015263,14.8370454, "Klub Patro");
+  addPinToMap(3,50.146487,14.9056533, "Kersko");
+  addPinToMap(4,50.1464659,14.8378023, "Skanzen v Přerově nad Labem");
+  addPinToMap(5,50.1806812,14.7936072, "Byšičky");
+  addPinToMap(6,50.2033323,14.8387577, "Barokní zámek a zahrada");
+}
+
+function addPinToMap(letter, lat, lon, title) {
+
+  var position = {lat: lat, lng: lon};
+  var pinImage = new google.maps.MarkerImage("https://chart.googleapis.com/chart?chst=d_map_pin_letter_withshadow&chld="+letter+"|004375|FFFFFF");
+
   var marker = new google.maps.Marker({
-    position: patroPos,
+    position: position,
     map: map,
-    title: "Klub Patro",
+    title: title,
     icon: pinImage
-    //icon: iconBase// + 'schools_maps.png'
   });
+
 }
